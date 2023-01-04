82234da8e3d88c7efe63039852da999d4256eb9b
Faizullah98 <faizibaltistani@gmail.com>
2023-01-04 23:17:42 +0500

    changes the html and js file

--- practice.js
+++ practice.js
@@ -1,1 +1,8 @@
-document.getElementById("demo").innerHTML= "Hello";
+let myArr = [1,2,3,4,["dog","hen","fish"]];
+ 
+document.getElementById("demo").innerHTML= myArr;
+
+function pshFunc(){
+    myArr.push([5,6,7,8]);
+    document.getElementById("demo").innerHTML= myArr;
+}
